5169d2dc3eb3f723718d24407e24c4ac44b99c6e
Nafersalas <nafersalas@gmail.com>
2016-05-20 14:00:39 -0500

    Actualizacón de la tabla tutores

--- app/Http/Routes/Tutores.php
+++ app/Http/Routes/Tutores.php
@@ -3,3 +3,5 @@
 //Route::get('tutores', 'Tutores\TutoresController@getAll');
 Route::post('tutores', 'Tutores\TutoresController@post');
 //Route::get('tutores/{tutor_id}', 'Tutores\TutoresController@get');
+//Route::put('tutores/{tutor_id}', 'Tutores\TutoresController@put');
+//Route::delete('requisitos/{tutor_id}', 'Tutores\TutoresController@delete');
