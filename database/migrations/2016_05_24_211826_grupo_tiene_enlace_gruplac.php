789ff35f4391b0730f02443e8ee8323be5c65491
Andrés Herrera García <andreshg112@gmail.com>
2016-05-24 17:28:34 -0500

    Actualización de Martín y de la tabla grupos.

---
+++ database/migrations/2016_05_24_211826_grupo_tiene_enlace_gruplac.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class GrupoTieneEnlaceGruplac extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('grupos', function (Blueprint $table) {
+            $table->string('enlace');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        $table->dropColumn('enlace');
+    }
+}
