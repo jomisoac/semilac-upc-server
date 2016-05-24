11deb9a2005979082be21cc3ec6b2f47676c2e10
REVENCH <REVENCH@DESKTOP-MN59KC7>
2016-05-24 14:25:11 -0500

    modelo semillero hecho

---
+++ app/Http/Controllers/Semillero/SemilleroController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers\Semillero;
+
+use App\Models\Semillero;
+use App\Models\Tutor;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Http\Requests;
+use Symfony\Component\HttpFoundation\JsonResponse;
+
+class SemilleroController extends Controller {
+
+    public function getAll() {
+        return Semillero::where('activo', '!=', 0)->get();
+    }
+
+    public function get($id) {
+        return $semillero = Semillero::find($id);
+    }
+/*
+    public function post(Request $request){
+        $data = $request->json()->all();
+        if($this->get($data['id'])){
+            return JsonResponse::create('Ya existe un semillero con este código');
+        }else{
+                $semillero = new Semillero($data);
+                if($semillero->save()){
+                    if ($semillero){
+                        return JsonResponse::create('Se creó el semillero correctamente.');
+                    }else{
+                        $semillero->delete();
+                        return JsonResponse::create('Ocurrió un error al guardar los datos.');
+                    }
+                }
+        }
+    }*/
+    public function post(Request $request) {
+        $data = $request->json()->all();
+        $semillero = new Semillero($data);
+        if ($semillero->save()) {
+            if ($semillero) {
+                return JsonResponse::create('Se creó el semillero correctamente.');
+            } else {
+                $semillero->delete();
+                return JsonResponse::create('Ocurrió un error al guardar los datos.');
+            }
+        }
+    }
+    
+
+    public function put(Request $request, $id) {
+        try {
+            $data = $request->json()->all();
+            $semillero = $this->get($id);
+            if ($semillero) {
+                foreach ($data as $campo => $valor) {
+                    $semillero->$campo = $valor;
+                }
+                if ($semillero->save()) {
+                    return JsonResponse::create('Semillero actualizado correctamente');
+                } else {
+                    return JsonResponse::create('No se pudo actualizar el semillero');
+                }
+            } else {
+                return JsonResponse::create('El semillero que desea modificar no existe');
+            }
+        } catch (Exception $e) {
+            return JsonResponse::create("Se produjo una exepcion");
+        }
+    }
+
+    //ESTE METODO NO ELMINA, SOLO DESACTIVA
+    public function delete($id) {
+        $semillero = $this->get($id);
+        if ($semillero) {
+            $semillero->activo = 0;
+            $semillero->save();
+            return JsonResponse::create('Semillero eliminado');
+        } else {
+            return JsonResponse::create('El semillero no existe');
+        }
+    }
+}
+
---
+++ database/migrations/2016_05_24_165325_crear_tabla_semilleros.php
@@ -0,0 +1,49 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CrearTablaSemilleros extends Migration {
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up() {
+        Schema::create('semilleros', function(Blueprint $table) {
+            $table->increments('id');
+            $table->integer('programa_id')->unsigned();
+            $table->integer('grupo_id')->unsigned();
+            $table->string('nombre');
+            $table->string('nombre_linea');
+            $table->string('objetivo_linea');
+            $table->string('logro_linea');
+            $table->string('efecto_linea');
+            $table->string('mision');
+            $table->string('vision');
+            $table->string('objetivo_general');
+            $table->string('objetivo_uno');
+            $table->string('objetivo_dos');
+            $table->string('objetivo_tres');
+            $table->string('estrategia_uno');
+            $table->string('estrategia_dos');
+            $table->string('estrategia_tres');
+            $table->integer('tutor_id')->unsigned();
+            $table->boolean('activo')->default('1');
+            $table->foreign('programa_id')->references('id')->on('programas');
+            $table->foreign('grupo_id')->references('id')->on('grupos');
+            $table->foreign('tutor_id')->references('id')->on('tutores');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down() {
+        Schema::drop('semilleros');
+    }
+
+}
