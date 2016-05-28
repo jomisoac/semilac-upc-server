a61960b2c6bcebcf1e2e5c93ba7bc9a7a3c5ee3e
Andrés Herrera García <andreshg112@gmail.com>
2016-05-28 15:50:33 -0500

    1. Se agregó una dependencia doctrine/dbal, para poder usar change() en las migraciones. Hacer 'composer install'. 2. Un tutor tiene solamente un grupo de investigación.

--- app/Models/Tutor.php
+++ app/Models/Tutor.php
@@ -7,14 +7,18 @@ use Illuminate\Database\Eloquent\Model;
 class Tutor extends Model
 {
     public $table = 'tutores';
-
+    
     public  $timestamps = false;
-
-    public $fillable = ['identificacion', 'nombres', 'apellidos', 'tipo_documento','sexo', 'fecha_expedicion', 'fecha_nacimiento',
-                        'cvlac','direccion','celular','email_personal','email','pass', 'usuario_id'];
+    
+    public $fillable = ['identificacion', 'nombres', 'apellidos', 'tipo_documento','sexo', 'fecha_expedicion', 'fecha_nacimiento', 'cvlac','direccion','celular','email_personal','email','pass', 'usuario_id'];
     
     public function usuario()
     {
         return $this->belongsTo(Usuario::class);
+    }
+    
+    public function grupo()
+    {
+        return $this->hasOne(Grupo::class);
     }
 }
