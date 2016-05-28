898349da80815c34d6036f1899957aa0c41acef6
Andrés Herrera García <andreshg112@gmail.com>
2016-05-28 12:05:52 -0500

    Corrección a la tabla mala de Nafer.

--- database/migrations/2016_05_26_133733_crear_tabla_invitaciones-semilleros.php
+++ database/migrations/2016_05_26_133733_crear_tabla_invitaciones-semilleros.php
@@ -6,27 +6,27 @@ use Illuminate\Database\Migrations\Migration;
 class CrearTablaInvitacionesSemilleros extends Migration
 {
     /**
-     * Run the migrations.
-     *
-     * @return void
-     */
+    * Run the migrations.
+    *
+    * @return void
+    */
     public function up()
     {
         Schema::create('invitaciones-semilleros', function (Blueprint $table) {
             $table->increments('id');
             $table->integer('tutor_id')->unsigned();
             $table->integer('estudiante_id')->unsigned();
-            $table->boolean('estado')->default('enviado', 'aceptado', 'rechazado');
+            $table->boolean('estado')->default(1);
             $table->foreign('tutor_id')->references('id')->on('tutores');
-            $table->foreign('estudiante_id')->references('id')->on('estudiante')->onDelete('cascade')->onUpdate('cascade');
+            $table->foreign('estudiante_id')->references('id')->on('estudiantes')->onDelete('cascade')->onUpdate('cascade');
         });
     }
-
+    
     /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
+    * Reverse the migrations.
+    *
+    * @return void
+    */
     public function down()
     {
         Schema::drop('invitaciones-semilleros');
