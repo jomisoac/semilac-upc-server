89ad276f1843dd0d3436af4a5e6bbd2dc9409f27
RicardoKaro <ricardo.caro9@hotmail.com>
2016-05-28 16:13:24 -0500

    se creo la migration de la tabala del estudiante envia solicitud a semilleros

---
+++ app/Models/EstudianteSolicitaSemillero.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class EstudianteSolicitaSemillero extends Model
+{
+    public $table = 'estudiante_solicita_semillero';
+
+    public  $timestamps = false;
+
+    public $fillable = ['estudiante_id','semillero_id','respuesta','activo'];
+
+    protected $guarded = ['id'];
+
+    public function estudiante()
+    {
+        return $this->hasOne(Estudiante::class);
+    }
+
+    public function semillero()
+    {
+        return $this->hasOne(Semillero::class);
+    }
+}
--- database/migrations/2016_05_28_171634_crear_tabla_solicitud_semilleros.php
+++ database/migrations/2016_05_28_171634_crear_tabla_solicitud_semilleros.php
@@ -12,12 +12,12 @@ class CrearTablaSolicitudSemilleros extends Migration
      */
     public function up()
     {
-        Schema::create('estudiante-solicita-semilleros', function (Blueprint $table) {
+        Schema::create('estudiante_solicita_semillero', function (Blueprint $table) {
             $table->increments('id');
             $table->integer('estudiante_id')->unsigned();
             $table->integer('semillero_id')->unsigned();
-            $table->enum('respuesta',['en espera','rechazada','aceptada']);
-            $table->boolean('estado')->default(1);            
+            $table->enum('respuesta',['en espera','rechazada','aceptada'])->default('en espera');
+            $table->boolean('activo')->default(1);
             $table->foreign('estudiante_id')->references('id')->on('estudiantes')->onDelete('cascade')->onUpdate('cascade');
             $table->foreign('semillero_id')->references('id')->on('semilleros');
         });
@@ -30,6 +30,6 @@ class CrearTablaSolicitudSemilleros extends Migration
      */
     public function down()
     {
-        Schema::drop('estudiante-solicita-semilleros');
+        Schema::drop('estudiante_solicita_semillero');
     }
 }
