5772ae43989321de6a97e28d4c12cf21833773f5
Jose M. Soto <jomisoac@gmail.com>
2016-05-30 15:30:33 -0500

    Correccion C56

--- app/Http/Controllers/Estudiantes/EstudianteController.php
+++ app/Http/Controllers/Estudiantes/EstudianteController.php
@@ -18,6 +18,18 @@ class EstudianteController extends Controller
         return $estudiante = Estudiante::find($id);
     }
 
+    public function estudiantesDisponibles(){
+        $estudiantes = array();
+        $datos = Estudiante::all()->load('proyectos');
+        foreach ($datos as $estudiante){
+            if(sizeof($estudiante->proyectos) == 0){
+                array_push($estudiantes, $estudiante);
+            }else{
+            }
+        }
+        return $estudiantes;
+    }
+
     function getUsuario($email){
         return Usuario::where('email', $email)->first();
     }
@@ -28,10 +40,10 @@ class EstudianteController extends Controller
             $usuario = $data['usuario'];
             unset($data['usuario']);
             if ($this->get($data['identificacion'])) {
-                return JsonResponse::create(array('mensaje'=>'Ya existe un estudiante con esta identificación', 'ok' => 'false'));
+                return JsonResponse::create(array('mensaje'=>'Ya existe un estudiante con esta identificación.', 'ok' => 'false'));
             } else {
                 if ($this->getUsuario($usuario['email'])) {
-                    return JsonResponse::create(array('mensaje'=>'Ya existe un usuario con este email', 'ok' => 'false'));
+                    return JsonResponse::create(array('mensaje'=>'Ya existe un usuario con este email.', 'ok' => 'false'));
                 } else {
                     $usuario = $this->createUser($usuario['email'], $usuario['pass']);
                     $data['usuario_id'] = $usuario->id;
@@ -39,19 +51,49 @@ class EstudianteController extends Controller
                     if ($estudiante->save()) {
                         if ($usuario) {
                             $this->addRol($usuario);
-                            return JsonResponse::create('Se registró correctamente, seras redireccionado a la página de inicio sesión.');
+                            return JsonResponse::create('Se registró correctamente. Serás redireccionado a la página de inicio de sesión.');
                         } else {
                             $estudiante->delete();
                             $usuario->delete();
-                            return JsonResponse::create(array('mensaje' => 'Ocurrió un error al guardar los datos, intentalo deneuvo.', 'ok' => 'false'));
+                            return JsonResponse::create(array('mensaje' => 'Ocurrió un error al guardar los datos. Inténtalo de nuevo.', 'ok' => 'false'));
                         }
                     }
                 }
             }
         } catch (Exception $e) {
-            return JsonResponse::create('Se produjo una exepción');
+            return JsonResponse::create('Se produjo una excepción.');
         }
     }
+
+    public function update(Request $request, $id)
+    {
+        try{
+            $data = $request->json()->all();
+            $estudiante = $this->get($id);
+
+            if($estudiante){
+//                actualizo los campos del director
+                foreach($data as $campo=>$valor){
+                    $estudiante->$campo = $valor;
+                }
+                if($estudiante->save()){
+                    return JsonResponse::create('Estudiante actualizado correctamente.');
+                }else {
+                    return JsonResponse::create('No se pudieron actualizar los datos del estudiante.');
+                }
+            }else{
+                return JsonResponse::create('El estudiante que desea modificar no existe.');
+            }
+
+        }catch(Exception $e){
+            return JsonResponse::create("Se produjo una excepción.");
+        }
+    }
+
+    public function createProyecto(Request $request){
+        var_dump(\MongoDB\BSON\toJSON($request));
+    }
+
 
     private function addRol($usuario)
     {
