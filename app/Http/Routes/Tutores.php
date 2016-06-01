383d3960c3cc4c0dafb73fb0524b002f90cf12c3
Nafersalas <nafersalas@gmail.com>
2016-06-01 16:11:32 -0500

    Tabla semillero-solicita-estudiante

--- app/Http/Routes/Tutores.php
+++ app/Http/Routes/Tutores.php
@@ -6,7 +6,7 @@ Route::get('tutores/{tutor_id}', 'Tutores\TutoresController@get');
 //Route::put('tutores/{tutor_id}', 'Tutores\TutoresController@put');
 //Route::delete('requisitos/{tutor_id}', 'Tutores\TutoresController@delete');
 
-
 //solicitudes
 Route::get('semillero_solicita_estudiante', 'Tutores\SemilleroSolicitaEstudianteController@getAll');
 Route::post('semillero_solicita_estudiante','Tutores\SemilleroSolicitaEstudianteController@post');
+
