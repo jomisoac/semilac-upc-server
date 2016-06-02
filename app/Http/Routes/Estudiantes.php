307ed97a715286a245c1e8368f198c7357aaed1c
Jose Miguel Soto Acosta <jomisoac@gmail.com>
2016-06-02 17:04:05 -0500

    Update and rename eSTUDIANTES.php to Estudiantes.php

--- app/Http/Routes/Estudiantes.php
+++ app/Http/Routes/Estudiantes.php
@@ -5,15 +5,17 @@
  * Time: 2:35 PM
  */
 
-Route::get('/estudiantes', 'Estudiantes\EstudianteController@getAll');
+//Route::get('/estudiantes', 'Estudiantes\EstudianteController@getAll');
+Route::get('/estudiantes/disponibles/{mi_id}', 'Estudiantes\EstudianteController@estudiantesDisponibles');
 Route::get('/estudiantes/disponibles', 'Estudiantes\EstudianteController@estudiantesDisponibles');
 Route::get('/estudiantes/{estudiante_id}', 'EstudianteController@get');
 Route::post('/estudiantes', 'Estudiantes\EstudianteController@post');
 Route::put('/estudiantes/{estudiante_id}', 'Estudiantes\EstudiantesController@update');
 Route::delete('/estudiantes/{estudiante_id}', 'Estudiantes\EstudiantesController@delete');
 
-Route::post('/estudiantes/nuevo_proyecto', 'EstudiantesController@createProyecto');
+Route::post('/estudiantes/nuevo_proyecto', 'Estudiantes\EstudianteController@createProyecto');
 
 //solicitudes
-Route::get('solicitud-semilleros', 'Estudiantes\EstudianteSolicitaSemilleroController@getAll');
+Route::get('solicitud-semilleros', 'Estudiantes\EstudianteSolicitaSemilleroController@estudiantesDisponibles');
 Route::post('solicitud-semilleros','Estudiantes\EstudianteSolicitaSemilleroController@post');
+
