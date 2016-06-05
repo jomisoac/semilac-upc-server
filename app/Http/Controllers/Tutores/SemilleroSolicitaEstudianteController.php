bbd5e45082d3cd0986a583071d16a419fb142338
RicardoKaro <ricardo.caro9@hotmail.com>
2016-06-05 14:42:57 -0500

    se habia norrado el actualizar

--- app/Http/Controllers/Tutores/SemilleroSolicitaEstudianteController.php
+++ app/Http/Controllers/Tutores/SemilleroSolicitaEstudianteController.php
@@ -13,7 +13,8 @@ use Symfony\Component\HttpFoundation\JsonResponse;
 
 class SemilleroSolicitaEstudianteController extends Controller
 {
-    public function getAll(){
+    public function getAll()
+    {
         return SemilleroSolicitaEstudiante::all();
     }
 
@@ -23,36 +24,65 @@ class SemilleroSolicitaEstudianteController extends Controller
             ->with(['semillero', 'semillero.tutor'])->get();
         return $solicitudes;
     }
-    
-    public function get($id){
+
+    public function get($id)
+    {
         return $solicitud = SemilleroSolicitaEstudiante::find($id);
     }
-    
-    function getUsuario($email){
+
+    function getUsuario($email)
+    {
         return Usuario::where('email', $email)->first();
     }
-    
-    public function post(Request $request){
+
+    public function post(Request $request)
+    {
         $data = $request->json()->all();
         //var_dump($request);
         $solicitud = new SemilleroSolicitaEstudiante();
         $solicitud->estudiante_id = $data['estudiante_id'];
         $solicitud->semillero_id = $data['semillero_id'];
-        
-        if($solicitud->save()){
-            if ($solicitud){
+
+        if ($solicitud->save()) {
+            if ($solicitud) {
                 //$this->guardarArchivo($request, $convocatoria->id);
                 $respuesta = array(
-                'mensaje' => 'Envio corréctamente la solicitud.',
-                'solicitud' => $solicitud
+                    'mensaje' => 'Envio corréctamente la solicitud.',
+                    'solicitud' => $solicitud
                 );
                 return JsonResponse::create($respuesta);
-            }else{
+            } else {
                 $solicitud->delete();
                 return JsonResponse::create('Ocurrió un error al guardar los datos.');
             }
         }
     }
+
+    public function put(Request $request, $id)
+    {
+        try {
+            $data = $request->json()->all();
+            $solicitud = $this->get($id);
+
+            if ($solicitud) {
+                //                actualizar los requisitos
+                foreach ($data as $campo => $valor) {
+                    $solicitud->$campo = $valor;
+                }
+                if ($solicitud->save()) {
+                    return JsonResponse::create('Solicitud actualizado correctamente');
+                } else {
+                    return JsonResponse::create('No se pudo actualizar la solicitud');
+                }
+            } else {
+                return JsonResponse::create('la solicitud que desea modificar no existe');
+            }
+        } catch (Exception $e) {
+            return JsonResponse::create("Se produjo una exepcion");
+        }
+    }
+
+
     private function getRol($nombre)
     {
         return Rol::where('nombre', $nombre)->first();
