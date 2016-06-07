ebaa55c7442e97a60923bf4c0080dfd02ef3ddbc
Andrés Herrera García <andreshg112@gmail.com>
2016-06-06 19:22:34 -0500

    Realizadas otras correcciones.

--- app/Http/Routes/EstudianteSolicitaSemillero.php
+++
@@ -1,4 +0,0 @@
-<?php
-Route::get('grupo', 'Director\GrupoController@getAll');
-Route::post('grupo', 'Director\GrupoController@post');
-Route::get('grupo/{grupo_id}', 'Director\GrupoController@get');
